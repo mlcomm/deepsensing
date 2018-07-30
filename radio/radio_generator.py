691f3a5cc9f9d792a4d99845f661c957875c4ea0
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-07-30 05:56:00 -0700

    Update radio_generator.py

--- radio/radio_generator.py
+++ radio/radio_generator.py
@@ -22,11 +22,15 @@ def qpsk_awgn_generator(batch_size = 4500, EbN0s = [-10, -8]):
         const = digital.qpsk_constellation()
         rrc_taps = firdes.root_raised_cosine(1, 4, 1, 0.35, ntaps)
         src = blocks.vector_source_b(map(int, np.random.randint(0, const.arity(), N_BITS/const.bits_per_symbol())), False)
+        
         mod = digital.chunks_to_symbols_bc((const.points()), 1)
         match_filter = filter.interp_fir_filter_ccc(4, (rrc_taps))
         amplitude = blocks.multiply_const_vcc((2, ))
+        
         noise_amplitude = 1.0 / math.sqrt(const.bits_per_symbol()* 10**(float(EbN0)/10))
+        #noise_amplitude = 1.0 / math.sqrt(10**(float(EbN0)/10))
         noise = analog.noise_source_c(analog.GR_GAUSSIAN, noise_amplitude, 0)
+        
         add = blocks.add_vcc(1)
         sink = blocks.vector_sink_c()
         noise_sink = blocks.vector_sink_c()
@@ -39,18 +43,20 @@ def qpsk_awgn_generator(batch_size = 4500, EbN0s = [-10, -8]):
         raw_noise_vector = np.array(noise_sink.data(), dtype=np.complex64)
 
         sampler_indx = random.randint(50, 500)
-        while sampler_indx + vec_length < len(raw_output_vector):
+        vec_indx = 0
+        while sampler_indx + vec_length < len(raw_output_vector) and vec_indx < nvecs_per_key:
             sampled_vector = raw_output_vector[sampler_indx:sampler_indx+vec_length]
             energy = np.sum((np.abs(sampled_vector)))
             sampled_vector = sampled_vector / energy
-            data[("busy", EbN0)][0,:] = np.real(sampled_vector)
-            data[("busy", EbN0)][1,:] = np.imag(sampled_vector)
+            data[("busy", EbN0)][vec_indx, 0,:] = np.real(sampled_vector)
+            data[("busy", EbN0)][vec_indx, 1,:] = np.imag(sampled_vector)
             sampled_noise_vector = raw_noise_vector[sampler_indx:sampler_indx+vec_length]
             energy = np.sum((np.abs(sampled_noise_vector)))
             sampled_noise_vector = sampled_noise_vector / energy
-            data[("idle", EbN0)][0,:] = np.real(sampled_noise_vector)
-            data[("idle", EbN0)][1,:] = np.imag(sampled_noise_vector)
-            sampler_indx += random.randint(vec_length, round(len(raw_output_vector)*.05))
+            data[("idle", EbN0)][vec_indx, 0,:] = np.real(sampled_noise_vector)
+            data[("idle", EbN0)][vec_indx, 1,:] = np.imag(sampled_noise_vector)
+            sampler_indx += vec_length
+            vec_indx += 1
         
     return data
     
