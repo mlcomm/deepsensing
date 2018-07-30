bc0cd3685b062eaeefacaad44b0a38301d66d11f
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-07-29 17:19:10 -0700

    Update network.py

--- nn/network.py
+++ nn/network.py
@@ -11,3 +11,24 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2
 from keras.regularizers import *
 from keras.optimizers import adam
 import random, sys, keras
+
+def sensing_network(dr = 0.5, in_shp = [2, 128]):
+    K.set_image_dim_ordering('th')
+    dr = 0.5
+    model = models.Sequential()
+    model.add(Reshape([1]+in_shp, input_shape=in_shp))
+    model.add(ZeroPadding2D((0, 2)))
+    model.add(Convolution2D(256, 1, 3, border_mode='valid', activation="relu", name="conv1", init='glorot_uniform'))
+    model.add(Dropout(dr))
+    model.add(ZeroPadding2D((0, 2)))
+    model.add(Convolution2D(80, 2, 3, border_mode="valid", activation="relu", name="conv2", init='glorot_uniform'))
+    model.add(Dropout(dr))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu', init='he_normal', name="dense1"))
+    model.add(Dropout(dr))
+    model.add(Dense( len(classes), init='he_normal', name="dense2" ))
+    model.add(Activation('softmax'))
+    model.add(Reshape([len(classes)]))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model.summary()
+    return model
