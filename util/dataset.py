abc70c922306e39fdeabeff2100b6135bc34a9aa
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-07-29 18:16:40 -0700

    Update dataset.py

--- util/dataset.py
+++ util/dataset.py
@@ -1,1 +1,8 @@
+import pickle
 
+def dataset_load(filepath):
+    [X_train, Y_train, X_test, Y_test] = pickle.load(open(filename,'rb'), encoding='latin1')
+    return [X_train, Y_train, X_test, Y_test]
+
+def dataset_save([X_train, Y_train, X_test, Y_test], filepath):
+    pickle.dump([X_train, Y_train, X_test, Y_test], file(filepath, "wb" ) )
