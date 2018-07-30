9786d58c71dacaa2ac5470ff2d702d082915226d
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-07-30 07:19:14 -0700

    Update radio_generator.py

--- radio/radio_generator.py
+++ radio/radio_generator.py
@@ -7,7 +7,7 @@ from gnuradio import blocks
 from gnuradio.filter import firdes
 from gnuradio import filter
 
-def qpsk_awgn_generator(batch_size = 4500, EbN0s = [-10, -8]):
+def qpsk_awgn_generator(batch_size = 4500, EbN0s = range(-20, 10, 2)):
     data = {}
     ntaps = 45
     N_BITS = 1000000
@@ -25,10 +25,10 @@ def qpsk_awgn_generator(batch_size = 4500, EbN0s = [-10, -8]):
         
         mod = digital.chunks_to_symbols_bc((const.points()), 1)
         match_filter = filter.interp_fir_filter_ccc(4, (rrc_taps))
-        amplitude = blocks.multiply_const_vcc((2, ))
+        amplitude = blocks.multiply_const_vcc((4, ))
         
-        noise_amplitude = 1.0 / math.sqrt(const.bits_per_symbol()* 10**(float(EbN0)/10))
-        #noise_amplitude = 1.0 / math.sqrt(10**(float(EbN0)/10))
+        #noise_amplitude = 1.0 / math.sqrt(const.bits_per_symbol()* 10**(float(EbN0)/10))
+        noise_amplitude = 1.0 / math.sqrt(10**(float(EbN0)/10))
         noise = analog.noise_source_c(analog.GR_GAUSSIAN, noise_amplitude, 0)
         
         add = blocks.add_vcc(1)
@@ -46,19 +46,16 @@ def qpsk_awgn_generator(batch_size = 4500, EbN0s = [-10, -8]):
         vec_indx = 0
         while sampler_indx + vec_length < len(raw_output_vector) and vec_indx < nvecs_per_key:
             sampled_vector = raw_output_vector[sampler_indx:sampler_indx+vec_length]
-            energy = np.sum((np.abs(sampled_vector)))
-            #sampled_vector = sampled_vector / energy
             data[("busy", EbN0)][vec_indx, 0,:] = np.real(sampled_vector)
             data[("busy", EbN0)][vec_indx, 1,:] = np.imag(sampled_vector)
             sampled_noise_vector = raw_noise_vector[sampler_indx:sampler_indx+vec_length]
-            energy = np.sum((np.abs(sampled_noise_vector)))
-            #sampled_noise_vector = sampled_noise_vector / energy
             data[("idle", EbN0)][vec_indx, 0,:] = np.real(sampled_noise_vector)
             data[("idle", EbN0)][vec_indx, 1,:] = np.imag(sampled_noise_vector)
             sampler_indx += vec_length
             vec_indx += 1
-        
+
     return data
     
 if __name__ == "__main__":
     qpsk_awgn_generator()
+
