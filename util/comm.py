25162480fbeeaf53e109f2f0022e069c99ae9e41
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-07-31 23:06:17 -0700

    Update comm.py

--- util/comm.py
+++ util/comm.py
@@ -41,4 +41,26 @@ def cal_roc(test_Y_hat):
       print(error.__class__.__name__ + ": " + error.message)
       
       
-      
+ def cal_pd_pf(X_test, Y_test, test_Y_hat):
+    Y_predict = np.zeros([len(Y_test),2])
+    for i in range(0,X_test.shape[0]):
+        k = int(np.argmax(test_Y_hat[i,:]))
+        Y_predict[i, k] = 1
+    pd = 0
+    pd_count = 0
+    pf = 0
+    px = 0
+    for i in range(0, X_test.shape[0]):
+        if Y_test[i,0] == 1:
+            pd_count += 1
+
+        if Y_test[i,0] == 1 and Y_predict[i,0] == 1:
+            pd += 1
+        if Y_test[i,0] == 0 and Y_predict[i,0] == 1:
+            pf += 1
+        if Y_test[i,0] == 0 and Y_predict[i,0] == 0:
+            px += 1
+            
+    pd = pd/(pd_count + 0.0)
+    pf = pf/(Y_test.shape[0]-pd_count + 0.0)
+    return pd, pf
