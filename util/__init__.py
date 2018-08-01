418f389956514c0187d4fdce7232ca8002cebc6e
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-07-31 22:27:56 -0700

    Update __init__.py

--- util/__init__.py
+++ util/__init__.py
@@ -1,4 +1,4 @@
 
 from util.dataset import dataset_load, dataset_save
 from util.energy_detector import ed_power_range, ed_pd_pf
-from util.comm import cal_roc
+from util.comm import cal_roc, plot_roc
